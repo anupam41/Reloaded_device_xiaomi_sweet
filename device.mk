aae6367a7e2084282a16a3821d8e848e9f104b44
Foxtrot47 <jjneutron@outlook.com>
2022-08-23 22:55:49 +0530

    sweet: Switch to QTI USB Component

--- device.mk
+++ device.mk
@@ -307,7 +307,8 @@ PRODUCT_COPY_FILES += \
 
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
-    perf
+    perf \
+    usb
     
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -315,10 +316,8 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
-    init.qcom.usb.sh \
     init.qti.dcvs.sh \
     init.qcom.rc \
-    init.qcom.usb.rc \
     init.sweet.rc \
     init.target.rc \
     ueventd.sweet.rc
@@ -380,14 +379,6 @@ PRODUCT_PACKAGES += \
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # vndservicemanager
 PRODUCT_PACKAGES += \
